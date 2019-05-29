0eca0b97f9b1f629189b9874b8725f5f71075983
Reginaldo <reginaldolribeiro@gmail.com>
2019-05-29 08:27:35 -0300

    Primeiro commit

---
+++ src/main/java/com/vinilcommerce/vinilcommerce/VinilcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.vinilcommerce.vinilcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinilcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinilcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinilcommerce/vinilcommerce/VinilcommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vinilcommerce.vinilcommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VinilcommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
