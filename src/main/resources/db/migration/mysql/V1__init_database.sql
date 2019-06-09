8a4ddb1d0b5a7c53f54346878af9329890ca48ba
Reginaldo <reginaldolribeiro@gmail.com>
2019-06-09 18:39:31 -0300

    Ajustes no script de carga do banco de dados

--- src/main/resources/db/migration/mysql/V1__init_database.sql
+++ src/main/resources/db/migration/mysql/V1__init_database.sql
@@ -46,39 +46,38 @@ CREATE TABLE `sale` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'POP', 25.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'POP', 7.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'POP', 6.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'POP', 2.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'POP', 10.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'POP', 15.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'POP', 20.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'POP', 25.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'POP', 7.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'POP', 6.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'POP', 2.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'POP', 10.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'POP', 15.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'POP', 20.00);
 
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'MPB', 30.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'MPB', 5.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'MPB', 10.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'MPB', 15.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'MPB', 20.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'MPB', 25.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'MPB', 30.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'MPB', 30.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'MPB', 5.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'MPB', 10.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'MPB', 15.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'MPB', 20.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'MPB', 25.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'MPB', 30.00);
 
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'CLASSIC', 35.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'CLASSIC', 3.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'CLASSIC', 5.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'CLASSIC', 8.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'CLASSIC', 13.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'CLASSIC', 18.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'CLASSIC', 25.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'CLASSIC', 35.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'CLASSIC', 3.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'CLASSIC', 5.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'CLASSIC', 8.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'CLASSIC', 13.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'CLASSIC', 18.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'CLASSIC', 25.00);
 
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'ROCK', 40.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'ROCK', 10.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'ROCK', 15.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'ROCK', 15.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'ROCK', 15.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'ROCK', 20.00);
-INSERT INTO `cashback`.`cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'ROCK', 40.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SUNDAY', 'ROCK', 40.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('MONDAY', 'ROCK', 10.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('TUESDAY', 'ROCK', 15.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('WEDNESDAY', 'ROCK', 15.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('THURSDAY', 'ROCK', 15.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('FRIDAY', 'ROCK', 20.00);
+INSERT INTO `cashback` (DAY_OF_WEEK, GENRE, VALUE) VALUES ('SATURDAY', 'ROCK', 40.00);
 
-INSERT INTO `cashback`.`customer` (`birth_date`, `email`, `name`) VALUES ('1985/08/22', 'reginaldolribeiro@gmail.com', 'Reginaldo Luiz Ribeiro Filho');
-INSERT INTO `cashback`.`customer` (`birth_date`, `email`, `name`) VALUES ('1965/10/20', 'fulanodasilva@gmail.com', 'Fulano da Silva');
-INSERT INTO `cashback`.`customer` (`birth_date`, `email`, `name`) VALUES ('1970/01/10', 'beltranosouza@gmail.com', 'Beltrano Souza');
-
+INSERT INTO `customer` (`birth_date`, `email`, `name`) VALUES ('1985/08/22', 'reginaldolribeiro@gmail.com', 'Reginaldo Luiz Ribeiro Filho');
+INSERT INTO `customer` (`birth_date`, `email`, `name`) VALUES ('1965/10/20', 'fulanodasilva@gmail.com', 'Fulano da Silva');
+INSERT INTO `customer` (`birth_date`, `email`, `name`) VALUES ('1970/01/10', 'beltranosouza@gmail.com', 'Beltrano Souza');
